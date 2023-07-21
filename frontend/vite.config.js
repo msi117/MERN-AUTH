4d4b7271d4d605a7d6d7d67b8c0437d52c50bf1e
Mir Sameer <mir.sameer@example.com>
2023-07-20 20:36:22 -0400

    Deployment Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://dry-wave-00338-94a07424ea60.herokuapp.com/',
         changeOrigin: true
       }
     }
